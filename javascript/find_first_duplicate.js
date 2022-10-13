94a4f0acdad46bcf339c2aeb9b3b0844ca08f559
Ian Iversen <ianiversen1@gmail.com>
2022-10-13 13:18:04 -0400

    as far as I got, need to learn uniques method

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,33 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  if (arr.length > 1) {
+    let x = 0
+    let y = arr.length - 1
+    let z = []
+    while (x <= y) {
+      arr
+      x++
+    }
+    return z
+  } else {
+    return -1
+  }
 }
+
+// function findFirstDuplicate(arr) {
+//   if (arr.length === 0, 1) {
+//     return (-1)
+//   }
+// }
+
+// function findFirstDuplicate(arr) {
+//   if (condition) {
+
+//   } else (condition){
+//     return -1
+//   }
+// }
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +44,10 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+//Ian's PSEUDOCODE
+
+//a random array is given as an arguement to a function, the function returns either one of two values:
+//EITHER the first duplicated number is returned
+//OR their are no duplicates return -1
